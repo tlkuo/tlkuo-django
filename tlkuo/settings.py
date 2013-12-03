4eb5087491a334593ee2092827e27d4b37403524
martin <tlkuo.frb@gmail.com>
2013-12-04 00:06:37 +0800

    add html minify middleware

--- tlkuo/settings.py
+++ tlkuo/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'htmlmin.middleware.HtmlMinifyMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
