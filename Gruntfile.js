f14a2c559215a5fe6cf62d9f1db427485e8078b7
martin <tlkuo.frb@gmail.com>
2013-12-04 17:01:30 +0800

    add grunt-contrib-htmlmin to Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,15 +4,40 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     // Metadata.
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    path: {
+      static_root: './static'
+    },
     // Task configuration.
+    htmlmin: {
+      // https://github.com/gruntjs/grunt-contrib-htmlmin
+      dist: {
+        options: {
+          removeComments: true,
+          removeCommentsFromCDATA: true,
+          removeCDATASectionsFromCDATA: true,
+          collapseWhitespace: true,
+          collapseBooleanAttributes: true,
+          removeAttributeQuotes: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          removeOptionalTags: true,
+          removeEmptyElements: true
+        },
+        expand: true,
+        cwd: '<%= path.static_root %>',
+        src: ['**/*.html'],
+        dest: '<%= path.static_root %>'
+      }
+    }
   });
 
   // These plugins provide necessary tasks.
-  // grunt.loadNpmTasks('');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
   // Default task.
   grunt.registerTask('default', []);
-  grunt.registerTask('production', []);
+  grunt.registerTask('production', ['htmlmin']);
 
 };
