6f3438ef627fce096c821df8de03a9d3a7b27455
martin <tlkuo.frb@gmail.com>
2013-12-05 11:46:39 +0800

    enable application "about"

--- tlkuo/settings.py
+++ tlkuo/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'github_hook',
+    'about',
 )
 
 MIDDLEWARE_CLASSES = (
--- tlkuo/urls.py
+++ tlkuo/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'tlkuo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^about/', include('about.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hook/', include('github_hook.urls')),
 )
