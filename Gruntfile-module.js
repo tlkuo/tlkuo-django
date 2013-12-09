26e4d4781e5f80d81c6738d34e2eb0472f7d2597
martin <tlkuo.frb@gmail.com>
2013-12-09 16:25:18 +0800

    use grunt-modules for each django applications

---
+++ Gruntfile-module.js
@@ -0,0 +1,46 @@
+// A module defines a function which takes in the grunt object 
+// and module name and returns a configuration object.
+module.exports = function(grunt, moduleName) {
+  var config = {},
+      static_module = ['.', moduleName, 'static'].join('/');
+
+  if (grunt.file.isDir(static_module)) {
+    config.htmlmin = {
+      // https://github.com/gruntjs/grunt-contrib-htmlmin
+      options: {
+        removeComments: true,
+        removeCommentsFromCDATA: true,
+        removeCDATASectionsFromCDATA: true,
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        removeAttributeQuotes: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeOptionalTags: true,
+        removeEmptyElements: false // experimental
+      },
+      expand: true,
+      cwd: static_module,
+      src: ['**/*.html'],
+      dest: '<%= path.static_root %>'
+    };
+
+    config.requirejs = {
+      // https://github.com/gruntjs/grunt-contrib-requirejs
+      options: {
+        name: moduleName + '.r',
+        context: null,
+        optimize: 'uglify2',
+        baseUrl: [static_module, moduleName, 'js'].join('/'),
+        mainConfigFile: [static_module, moduleName, 'js', moduleName + '.r.js' ].join('/'),
+        out: ['<%= path.static_root %>', moduleName, 'js', moduleName + '.r.js'].join('/')
+      }
+    };
+
+    if (!grunt.file.isFile(config.requirejs.options.mainConfigFile))
+        delete config.requirejs;
+  }
+
+  return config;
+};
--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,6 @@
 /*global module:false*/
+var gruntModules = require('grunt-modules');
+
 module.exports = function(grunt) {
 
   // Project configuration.
@@ -7,45 +9,9 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     path: {
       static_root: './static',
-      static_about: './about/static',
       static_jam: './jam/static'
     },
     // Task configuration.
-    htmlmin: {
-      // https://github.com/gruntjs/grunt-contrib-htmlmin
-      dist: {
-        options: {
-          removeComments: true,
-          removeCommentsFromCDATA: true,
-          removeCDATASectionsFromCDATA: true,
-          collapseWhitespace: true,
-          collapseBooleanAttributes: true,
-          removeAttributeQuotes: true,
-          removeRedundantAttributes: true,
-          useShortDoctype: true,
-          removeEmptyAttributes: true,
-          removeOptionalTags: true,
-          removeEmptyElements: false // experimental
-        },
-        expand: true,
-        cwd: '<%= path.static_root %>',
-        src: ['**/*.html'],
-        dest: '<%= path.static_root %>'
-      }
-    },
-    requirejs: {
-      // https://github.com/gruntjs/grunt-contrib-requirejs
-      compile: {
-        options: {
-          name: 'about.r',
-          context: null,
-          optimize: 'uglify2',
-          baseUrl: '<%= path.static_about %>' + '/about/js',
-          mainConfigFile: '<%= path.static_about %>' + '/about/js/about.r.js',
-          out: '<%= path.static_root %>' + '/about/js/about.r.js'
-        }
-      }
-    },
     jam: {
       // https://github.com/shama/grunt-jam
       dist: {
@@ -58,6 +24,10 @@ module.exports = function(grunt) {
         }
       }
     }
+  });
+
+  gruntModules(grunt, {
+    about: require('./Gruntfile-module.js')
   });
 
   // These plugins provide necessary tasks.
